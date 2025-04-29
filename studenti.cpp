34e2ad61e370599783364306087281731945fd31
adisqajani1 <adis.qajani@student.uni-pr.edu>
2025-04-29 21:09:37 +0200

    Update studenti.cpp

--- studenti.cpp
+++ studenti.cpp
@@ -9,6 +9,22 @@ struct Studenti {
     int mosha;
     float notaMesatare;
 };
+// Funksioni per shfaqjen e te gjithe studenteve
+void shfaqStudentet(const Studenti studentet[], int numriStudentave) {
+    if (numriStudentave == 0) {
+        cout << "Nuk ka studente te regjistruar.";
+        return;
+    }
+    cout << "\n--- Lista e Studenteve ---\n";
+    for (int i = 0; i < numriStudentave; i++) {
+        cout << "Studenti #" << (i + 1) << endl;
+        cout << "Emri: " << studentet[i].emri << endl;
+        cout << "Mbiemri: " << studentet[i].mbiemri << endl;
+        cout << "Mosha: " << studentet[i].mosha << endl;
+        cout << "Nota mesatare: " << studentet[i].notaMesatare << "\n";
+    }
+}
+
 int main(){
   return 0;
         }
