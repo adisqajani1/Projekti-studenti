a743e6f777084ba411181c4bcbb714a0e5ff1ba7
adisqajani1 <adis.qajani@student.uni-pr.edu>
2025-04-29 21:13:05 +0200

    Update studenti.cpp

--- studenti.cpp
+++ studenti.cpp
@@ -9,6 +9,16 @@ struct Studenti {
     int mosha;
     float notaMesatare;
 };
+// Funksioni per menune
+void menu() {
+    cout << "\n--- MENU ---\n";
+    cout << "1. Regjistro student\n";
+    cout << "2. Shfaq te gjithe studentet\n";
+    cout << "3. Kerko student\n";
+    cout << "4. Dil nga programi\n";
+    cout << "Zgjedhja juaj: ";
+}
+    
 int main(){
   return 0;
         }
