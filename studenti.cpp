5e7d6c143383567dc2a94e6e0745ad8b9a5c95d9
adisqajani1 <adis.qajani@student.uni-pr.edu>
2025-04-29 21:05:49 +0200

    Create studenti.cpp

---
+++ studenti.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Struktura per student
+struct Studenti {
+    string emri;
+    string mbiemri;
+    int mosha;
+    float notaMesatare;
+};
+int main(){
+  return 0;
+        }
