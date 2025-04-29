f5f7203ff3274606f0a32fadae484585c307d111
adisqajani1 <adis.qajani@student.uni-pr.edu>
2025-04-29 21:11:15 +0200

    Update studenti.cpp

--- studenti.cpp
+++ studenti.cpp
@@ -9,6 +9,30 @@ struct Studenti {
     int mosha;
     float notaMesatare;
 };
+// Funksioni per kerkim te studentit
+void kerkoStudent(const Studenti studentet[], int numriStudentave) {
+    string emerKerkim;
+    cout << "Shkruaj emrin e studentit qe deshiron te kerkosh: ";
+    cin >> emerKerkim;
+
+    bool gjetur = false;
+
+    for (int i = 0; i < numriStudentave; i++) {
+        if (studentet[i].emri == emerKerkim) {
+            cout << "\nStudenti u gjet!\n";
+            cout << "Emri: " << studentet[i].emri << endl;
+            cout << "Mbiemri: " << studentet[i].mbiemri << endl;
+            cout << "Mosha: " << studentet[i].mosha << endl;
+            cout << "Nota mesatare: " << studentet[i].notaMesatare << "\n";
+            gjetur = true;
+            break;
+        }
+    }
+    if (!gjetur) {
+         cout << "Nuk u gjet asnje student me kete emer.\n";
+    }
+}
+
 int main(){
   return 0;
         }
