e1b95d155eabc7ef5c7256a9511c4d152c9e7a1f
adisqajani1 <adis.qajani@student.uni-pr.edu>
2025-04-29 21:20:41 +0200

    Update studenti.cpp

--- studenti.cpp
+++ studenti.cpp
@@ -25,22 +25,6 @@ void shfaqStudentet(const Studenti studentet[], int numriStudentave) {
         cout << "Nota mesatare: " << studentet[i].notaMesatare << "\n";
     }
 
-
-// Funksioni per regjistrimin e nje studenti
-void regjistroStudent(Studenti studentet[], int &numriStudentave) {
-    
-    cout << "Shkruaj emrin e studentit: ";
-    cin >> studentet[numriStudentave].emri;
-    cout << "Shkruaj mbiemrin e studentit: ";
-    cin >> studentet[numriStudentave].mbiemri;
-    cout << "Shkruaj moshen e studentit: ";
-    cin >> studentet[numriStudentave].mosha;
-    cout << "Shkruaj noten mesatare te studentit: ";
-    cin >> studentet[numriStudentave].notaMesatare;
-    numriStudentave++;
-
-}
-
 int main(){
   return 0;
         }
